cd8ed7f85c1ddbb0d38cdfe8d6cab7ab6a3980e2
N-Kamath <83540953+N-Kamath@users.noreply.github.com>
2023-10-28 04:59:45 -0500

    Update script.js

--- Lab3/script.js
+++ Lab3/script.js
@@ -1,28 +1,41 @@
+
+const sliders = document.querySelectorAll("input[type='range']");
 const billInput = document.getElementById("bill");
-const tipRange = document.getElementById("tip");
-const tipPercentOutput = document.getElementById("tip-percent");
-const tipAmountOutput = document.getElementById("tip-amount");
-const totalAmountOutput = document.getElementById("total-amount");
+sliders.forEach(function(slider){
 
-billInput.addEventListener("input", calculateTip);
-tipRange.addEventListener("input", calculateTip);
+    slider.addEventListener("input",calculateTip);
+});
 
-function calculateTip() {
-  const bill = parseFloat(billInput.value);
-  const tipPercent = tipRange.value;
+billInput.addEventListener("change",calculateTip);
 
-  if (isNaN(bill)) {
-    alert("Enter a valid input");
-    billInput.value = "";
-  } else {
-    const totalTip = (bill * tipPercent) / 100;
-    const total = bill + totalTip;
 
-    billInput.value = bill.toFixed(2);
-    tipPercentOutput.textContent = tipPercent + "%";
-    tipAmountOutput.textContent = totalTip.toFixed(2) + "$";
-    totalAmountOutput.textContent = total.toFixed(2) + "$";
-  }
+function calculateTip(){
+    var bill1 = parseFloat(billInput.value);
+    let tipPercent = document.getElementById("tip").value;
+   
+
+    billInput.value = bill1.toFixed(2);
+  
+    if ( isNaN(billInput.value) ) {
+        
+        alert('Enter valid Input');
+        billInput.value=0;
+       
+        
+       
+    } else {
+       
+    let totalTip = parseFloat((bill1 * (tipPercent/100)).toFixed(2));
+    let total = parseFloat((bill1 + totalTip).toFixed(2));
+
+
+
+    document.getElementById("tip-amount").textContent = `${totalTip}$`;
+    document.getElementById("total-amount").textContent = `${total}$`;
+    
+    document.getElementById("tip-percent").textContent = `${tipPercent}%`;
+  
+    }
 }
 
 calculateTip();
